67c0cf5b4060035474da6a0448790d9c600f0a59
yamen32123 <69954554+yamen32123@users.noreply.github.com>
2020-08-20 08:35:07 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("744377755407351849")
+setInterval(function() {
+channel.send(`goodmorning_lollololo`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
